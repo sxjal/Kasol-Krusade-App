a200bfc80880924887609d556ec6916c847f084e
sajal <sajalsahu8@gmail.com>
2024-03-06 13:00:41 +0530

    updaessssssssss

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   State<MainScreen> createState() => _MainScreenState();
@@ -10,23 +11,45 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  List<Map<String, dynamic>> _bookings = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchBookings();
+  }
+
+  void _fetchBookings() async {
+    try {
+      QuerySnapshot querySnapshot =
+          await _firestore.collection('bookings').get();
+      setState(() {
+        _bookings = querySnapshot.docs
+            .map((doc) => doc.data())
+            .toList()
+            .cast<Map<String, dynamic>>();
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            try {
-              await _auth.signOut();
-              Navigator.of(context)
-                  .pop(); // Navigate back to the previous screen
-            } catch (e) {
-              print(e);
-            }
-          },
-          child: const Text("Logout"),
-        ),
+        child: _bookings.isEmpty
+            ? CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: _bookings.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_bookings[index]['name']),
+                    subtitle: Text(_bookings[index]['email']),
+                  );
+                },
+              ),
       ),
     );
   }

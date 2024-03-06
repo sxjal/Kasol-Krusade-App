686d5444a371381c68f3833fe8abd897feeec8ee
sajal <sajalsahu8@gmail.com>
2024-03-06 12:42:19 +0530

    updatyed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:kasolkrusade/home.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -39,48 +40,126 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(1, 14, 47, 65),
+      backgroundColor: Color.fromARGB(1, 39, 122, 166), //Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Image()
+            const Image(
+              image: AssetImage("assets/kasolkrusade.png"),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
             TextField(
               keyboardType: TextInputType.emailAddress,
               onChanged: (value) {
                 email = value;
               },
               decoration: const InputDecoration(
-                hintText: 'Enter your email',
-                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                hintText: 'Email',
+                hintStyle: TextStyle(color: Colors.white24),
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                  ),
+                ),
               ),
+              style: const TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+              ), // Change this to your desired color
             ),
             const SizedBox(height: 20.0),
             TextField(
-              obscureText: true,
+              obscureText: hidden,
               onChanged: (value) {
                 password = value;
               },
-              decoration: const InputDecoration(
-                hintText: 'Enter your password',
-                border: OutlineInputBorder(),
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  color: Colors.white,
+                  onPressed: () {
+                    setState(
+                      () {
+                        hidden = !hidden;
+                      },
+                    );
+                  },
+                  icon: Icon(
+                    hidden ? Icons.visibility_off : Icons.visibility,
+                    color: Theme.of(context).primaryColorDark,
+                  ),
+                ),
+                hintText: 'Password?',
+                hintStyle: const TextStyle(
+                  color: Colors.white24,
+                ),
+                border: const OutlineInputBorder(),
               ),
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              // Change this to your desired color
             ),
             const SizedBox(height: 20.0),
-            ElevatedButton(
-              child: const Text('Log In'),
-              onPressed: () async {
+            GestureDetector(
+              onTap: () async {
+                if (email == null || email.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Please enter an email')),
+                  );
+                  return;
+                }
+                if (!email.contains('@')) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Please enter a valid email')),
+                  );
+                  return;
+                }
+                if (password == null || password.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Please enter a password')),
+                  );
+                  return;
+                }
+                if (password.length < 6) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                        content: Text(
+                            'Password must be at least 6 characters long')),
+                  );
+                  return;
+                }
                 try {
                   final user = await _auth.signInWithEmailAndPassword(
                       email: email, password: password);
                   if (user != null) {
-                    // Navigate to the next screen
+                    //navigate to another screen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MainScreen()),
+                    );
+                 
                   }
                 } catch (e) {
-                  print(e);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Error: $e')),
+                  );
                 }
               },
+              child: Container(
+                color: Colors.white,
+                height: MediaQuery.of(context).size.height * .05,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  "Login",
+                ),
+              ),
             ),
           ],
         ),
